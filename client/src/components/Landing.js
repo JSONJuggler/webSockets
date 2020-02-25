2e95c4f3fb2ecf5fde93e83171da2e8ce06a0350
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-24 22:58:49 -0600

    after having sockets only connect upon first render, emits and broadcasts function as expected...except when more than one websocket is broadcasting to another websocket (min 3 websockets)

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import io from "socket.io-client";
 
 export default function Landing() {
-  const socket = io("http://localhost:5000");
+  const [currentSocket, setCurrentSocket] = useState(null);
 
-  return (
-    <div id="mario-chat">
-      <div id="chat-window">
-        <div id="output"></div>
-        <div id="feedback"></div>
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+    setCurrentSocket(socket);
+    // return () => {
+    //   socket.disconnect();
+    // };
+    console.log(currentSocket);
+  }, []);
+
+  const [formData, setFormData] = useState({
+    handle: "",
+    message: ""
+  });
+
+  const [recieving, setReceiving] = useState(false);
+  const [feedback, setFeedback] = useState([]);
+
+  if (currentSocket) {
+    const handleHandle = e => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleChange = e => {
+      // console.log(e.target.name);
+      // console.log(e.target.value);
+      currentSocket.emit("typing", formData.handle);
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    currentSocket.on("typing", function(data) {
+      setReceiving(true);
+      setFeedback(
+        <p>
+          <em>{data} is typing </em>
+        </p>
+      );
+    });
+    return (
+      <div id="mario-chat">
+        <div id="chat-window">
+          <div id="output"></div>
+          {recieving && <div id="feedback">{feedback}</div>}
+        </div>
+        <input
+          id="handle"
+          name={"handle"}
+          type="text"
+          placeholder="Handle"
+          onChange={e => handleHandle(e)}
+        />
+        <input
+          id="message"
+          name={"message"}
+          type="text"
+          placeholder="Message"
+          onChange={e => handleChange(e)}
+        />
+        <button id="send">Send</button>
       </div>
-      <input id="handle" type="text" placeholder="Handle" />
-      <input id="message" type="text" placeholder="Message" />
-      <button id="send">Send</button>
-    </div>
-  );
+    );
+  } else {
+    return <Fragment>Problem connecting to server</Fragment>;
+  }
 }
