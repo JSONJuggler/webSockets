7e4f82742f643583c7bc8f5a1623bf686d94b3dd
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-25 01:52:14 -0600

    underscoreJS to the rescue!

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, Fragment } from "react";
 import io from "socket.io-client";
+import _ from "underscore";
 
 export default function Landing() {
   const [currentSocket, setCurrentSocket] = useState(null);
@@ -28,33 +29,63 @@ export default function Landing() {
     const handleChange = e => {
       // console.log(e.target.name);
       // console.log(e.target.value);
-      currentSocket.emit("typing", formData.handle);
+      currentSocket.emit("typing", {
+        handle: formData.handle,
+        id: currentSocket.id
+      });
       setFormData({ ...formData, [e.target.name]: e.target.value });
     };
+
     currentSocket.on("typing", function(data) {
       setReceiving(true);
+      // console.log(data);
       setFeedback(prevFeedback => {
-        if (
-          prevFeedback.forEach(feedback => {
-            if (feedback.props.children[0] === data) {
-              return true;
-            } else {
-              return false;
-            }
-          })
-        ) {
+        if (_.findWhere(prevFeedback, { key: data.id })) {
           return prevFeedback;
         } else {
           return [
             ...prevFeedback,
-            <p>
-              <em>{data} is typing </em>
+            <p key={data.id}>
+              <em>{data.handle} is typing </em>
             </p>
           ];
         }
       });
-      console.log(feedback);
+      // setFeedback(prevFeedback => {
+      //   if (prevFeedback.length === 0) {
+      //     return [
+      //       ...prevFeedback,
+      //       <p key={data.id}>
+      //         <em>{data.handle} is typing </em>
+      //       </p>
+      //     ];
+      //   }
+      //   if (prevFeedback.length > 0) {
+      //     console.log("hi");
+
+      //     // prevFeedback.forEach(fb => {
+      //     for (let i = 0; i < prevFeedback.length; i++) {
+      //       if (prevFeedback[i].key === data.id) {
+      //         console.log(prevFeedback[0].key);
+
+      //         console.log("u da same");
+      //         return setFeedback(prevFeedback => prevFeedback);
+      //       } else {
+      //         console.log("u diff");
+
+      //         return setFeedback(prevFeedback => [
+      //           ...prevFeedback,
+      //           <p key={data.id}>
+      //             <em>{data.handle} is typing </em>
+      //           </p>
+      //         ]);
+      //       }
+      //     }
+      //     // });
+      //   }
+      // });
     });
+    console.log(feedback);
     return (
       <div id="mario-chat">
         <div id="chat-window">
